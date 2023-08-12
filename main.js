1bffa902d86240149f321ad13d1b483807a4a48e
sergei_radaev <pringles777@mail.ru>
2023-08-12 21:51:53 +0300

    1

--- main.js
+++ main.js
@@ -147,8 +147,8 @@ class Card {
     this.imgBack = document.createElement('img');
     this.img.classList.add('rounded', 'img-fluid', 'd-none');
     this.imgBack.classList.add('rounded', 'img-fluid', 'imgBack');
-    this.img.src = '/bg/' + this.cardNumber + '.jpg';
-    this.imgBack.src = '/bg/0.jpg';
+    this.img.src = 'bg/' + this.cardNumber + '.jpg';
+    this.imgBack.src = 'bg/0.jpg';
     this.imgBack.style.cssText = 'cursor: pointer;'
     card.appendChild(this.img);
     card.appendChild(this.imgBack);
